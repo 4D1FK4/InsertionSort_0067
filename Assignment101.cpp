b7a2ad957357628b21d0a048ffae1846f73ce30d
4D1FK4 <adifkapersonal0812@gmail.com>
2026-03-12 11:04:51 +0700

    Terms or smth

--- Assignment101.cpp
+++ Assignment101.cpp
@@ -8,3 +8,11 @@ void input() {  // procedure input
     while (true) {
         cout << "Masukan jumlah data Pada Array : ";
         cin >> n;
+
+             if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray tidak boleh lebih dari 20\n";
+        }
+    }
