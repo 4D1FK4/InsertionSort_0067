449a398d42a1cfc5f96b1c177baf6a1cd1df9225
4D1FK4 <adifkapersonal0812@gmail.com>
2026-03-12 11:02:29 +0700

    INput1

--- Assignment101.cpp
+++ Assignment101.cpp
@@ -3,3 +3,8 @@ using namespace std;
 
 int arr[20];    // membuat array dengan panjang data 20
 int n;          // membuat variabel inputan n
+
+void input() {  // procedure input
+    while (true) {
+        cout << "Masukan jumlah data Pada Array : ";
+        cin >> n;
